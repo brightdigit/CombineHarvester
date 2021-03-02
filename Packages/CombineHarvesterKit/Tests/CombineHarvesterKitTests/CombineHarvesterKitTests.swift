c504dbe609f578f7c30ee9fca0ea116d2cce8db5
Leo Dion <leogdion@brightdigit.com>
2021-03-02 14:30:45 -0500

    fixing linting issue with test

--- Packages/CombineHarvesterKit/Tests/CombineHarvesterKitTests/CombineHarvesterKitTests.swift
+++ Packages/CombineHarvesterKit/Tests/CombineHarvesterKitTests/CombineHarvesterKitTests.swift
@@ -6,7 +6,7 @@ final class CombineHarvesterKitTests: XCTestCase {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct
     // results.
-    //XCTAssertEqual(CombineHarvesterKit().text, "Hello, World!")
+    // XCTAssertEqual(CombineHarvesterKit().text, "Hello, World!")
   }
 
   static var allTests = [
