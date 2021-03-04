8e201510be6017fbd8b09ebda0a4b5e3b6b3af8e
Leo Dion <leogdion@brightdigit.com>
2021-03-04 11:48:46 -0500

    added form

--- Packages/CombineHarvesterKit/Sources/CombineHarvesterKit/Views/BaconView.swift
+++ Packages/CombineHarvesterKit/Sources/CombineHarvesterKit/Views/BaconView.swift
@@ -10,73 +10,116 @@ import Combine
 
 public struct BaconSentence : Identifiable {
   public init(sentence: String) {
-    self.id = Date()
+    self.date = Date()
     self.sentence = sentence
   }
   
-  public let id : Date
+  public let date : Date
   public let sentence: String
+  
+  public var id: Date {
+    return self.date
+  }
 }
 
 public class BaconObject: ObservableObject {
   @Published var value : Int
   @Published var words = [BaconSentence]()
   
-  let maxSize : Int = 10
-  
-  var increment = PassthroughSubject<Void, Never>()
+  @Published var requestSize : Int = 1
+  @Published var maxSize : Int = 10
+  @Published var seconds : TimeInterval = 1
   
   var cancellable : AnyCancellable!
+  var timerCancellable : Cancellable!
   
-  static let url = URL(string: "https://baconipsum.com/api/?type=all-meat&sentences=1&format=json")!
+  static let baseURL = URLComponents(string: "https://baconipsum.com/api/?type=all-meat&format=json")!
   
   public init (timerInterval: TimeInterval = 1.0) {
     self.value = 0
     
     let urlSession = URLSession.shared
     
-    let urlPublisher = urlSession.dataTaskPublisher(for: Self.url)
-      .map(\.data)
-      .decode(type: [String].self, decoder: JSONDecoder())
-      .replaceError(with: [String]())
-      .flatMap{$0.publisher}
-      .map(BaconSentence.init(sentence:))
+    let urlPublisher =  $requestSize.compactMap { (size) -> URL? in
+      var newURL = Self.baseURL
+      newURL.queryItems?.append(URLQueryItem(name: "sentences", value: size.description))
+      return newURL.url
+    }.flatMap(urlSession.dataTaskPublisher(for:)).map(\.data)
+    .decode(type: [String].self, decoder: JSONDecoder())
+    .replaceError(with: [String]())
+    .flatMap{$0.publisher}
+    .map(BaconSentence.init(sentence:))
     
-    let wordsPublisher = Timer.publish(every: timerInterval, on: .current, in: .default)
-      .autoconnect()
-      .flatMap {_ in urlPublisher }
-      .map({ (newWord) -> [BaconSentence] in
-        var words = self.words
-        if words.count > self.maxSize {
-          let range = (self.maxSize...words.count-1)
-          words.removeSubrange(range)
-        }
-        words.insert(newWord, at: 0)
-        return words
-      })
-      
+    let timerPublisher =  self.$seconds.flatMap{ (seconds) -> Timer.TimerPublisher in
+      if let cancellable = self.timerCancellable {
+        cancellable.cancel()
+      }
+      let publisher = Timer.publish(every: seconds, on: .current, in: .default)
+      self.timerCancellable = publisher.connect()
+      return publisher
+    }
+    
+    let wordsPublisher = timerPublisher.flatMap{_ in urlPublisher}.combineLatest(self.$maxSize, { (newWord, maxSize) -> [BaconSentence] in
+      var words = self.words
+      words.insert(newWord, at: 0)
+      if words.count > self.maxSize {
+        let range = (self.maxSize-1...words.count-1)
+        words.removeSubrange(range)
+      }
+      return words
+    })
+    
     wordsPublisher.receive(on: DispatchQueue.main).print().assign(to: &self.$words)
     
-    cancellable = increment.receive(on: DispatchQueue.main).sink{
-        self.value += 1
-    }
+    
   }
 }
 
 struct BaconView: View {
+  let dateFormatter : DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .long
+    return formatter
+  }()
+  
   @EnvironmentObject var object : BaconObject
   
-    var body: some View {
+  var body: some View {
+    VStack{
+      Form{
+        Section{
+          HStack{
+            Text("Update every")
+            Text(object.seconds.description)
+            Stepper("seconds", value: $object.seconds, in: 0.5...10, step: 0.25)
+          }
+          HStack{
+            Text("Keep upto")
+            Text(object.maxSize.description)
+            Stepper("items", value: $object.maxSize, in: 1...20, step: 1)
+          }
+          HStack{
+            Text("Request upto")
+            Text(object.requestSize.description)
+            Stepper("items", value: $object.requestSize, in: 1...20, step: 1)
+          }
+        }
+      }
       VStack{
-        List(object.words) {
-          Text($0.sentence)
+        List(object.words) { (item) in
+          VStack(alignment: .leading) {
+            Text(item.sentence)
+            Text(self.dateFormatter.string(from: item.date)).font(.caption).opacity(0.75)
+          }
         }.animation(.easeIn)
       }
     }
+  }
 }
 
 struct BaconView_Previews: PreviewProvider {
-    static var previews: some View {
-      BaconView().environmentObject(BaconObject())
-    }
+  static var previews: some View {
+    BaconView().environmentObject(BaconObject())
+  }
 }
