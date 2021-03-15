a37868ea53f9c78cbdc14792adba194f8e6667d1
Leo Dion <leogdion@brightdigit.com>
2021-03-15 14:22:25 -0400

    fixing linting issue

--- Packages/CombineHarvesterKit/Package.swift
+++ Packages/CombineHarvesterKit/Package.swift
@@ -39,10 +39,10 @@ let package = Package(
   // let requiredCoverage: Int = 1
 
   let config = PackageConfiguration([
-    "rocket" : ["steps":
-            [
-                "hide_dev_dependencies"
-            ]],
+    "rocket": ["steps":
+      [
+        "hide_dev_dependencies"
+      ]],
     "komondor": [
       //      "pre-push": [
 //        //"swift test --enable-code-coverage --enable-test-discovery",
